823650eb5b8266e785d634e7c2bd27b3bfd4aa2b
Stefano Miceli <mic@nucleode.com>
2022-12-14 13:55:40 +0100

    initial commit

---
+++ functions-test.js
@@ -0,0 +1,26 @@
+// function myFunc1(str) {
+//   console.log(str);
+// }
+
+// const myFunc1 = function (str) {
+//   const withEnthusiasm = str + "!!!!";
+
+//   return withEnthusiasm;
+// };
+
+// const myFunc2 = str => {
+//   return str + "!!!!";
+// };
+
+// const myFunc3 = str => str + "!!!!";
+
+// console.log("outer console log");
+
+// console.log(myFunc1("inner console log"));
+// myFunc1("log");
+// myFunc1("console log");
+// myFunc1("inner log");
+
+// const computedValue = myFunc3("Ciao Epicoders"); // inner console log!!!
+
+// console.log(computedValue);
---
+++ index.js
@@ -0,0 +1,3 @@
+console.log(document.getElementsByClassName("list-item"));
+
+console.log(window);
